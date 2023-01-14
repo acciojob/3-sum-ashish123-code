9c9ba73ab9ffc0e40f711b1a43d509d70c9634cd
Ashish kumar Sahoo <kumarsahoo912@gmail.com>
2023-01-14 22:05:40 +0530

    3Sum

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 function threeSum(arr, target) {
   //your code here
-}
+ 
+    arr.sort((a, b) => a - b);
+    let closest = Infinity;
+    let result = 0;
+  
+    for (let i = 0; i < arr.length - 2; i++) {
+      let j = i + 1;
+      let k = arr.length - 1;
+  
+      while (j < k) {
+        let sum = arr[i] + arr[j] + arr[k];
+        let diff = Math.abs(sum - target);
+  
+        if (diff < closest) {
+          closest = diff;
+          result = sum;
+        }
+  
+        if (sum < target) {
+          j++;
+        } else if (sum > target) {
+          k--;
+        } else {
+          return sum;
+        }
+      }
+    }
+  
+    return result;
+  }
+  
 
-module.exports = threeSum;
+
+// module.exports = threeSum;
+let S = [-1, 2, 1, -4];
+let target = 1;
+console.log(threeSum(S, target)); 
